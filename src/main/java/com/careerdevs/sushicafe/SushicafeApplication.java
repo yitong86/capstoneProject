ea5e6e26d19d6745746ac66ddc21793fce51d77c
yitong86 <sophiajiang86@hotmail.com>
2022-07-03 21:40:08 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/sushicafe/SushicafeApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sushicafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushicafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushicafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/sushicafe/SushicafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sushicafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushicafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
